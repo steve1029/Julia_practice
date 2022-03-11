a2f2b7414c08f3a76755316785fdcc228939afa4
DongGun Lee <zlffj1032@gmail.com>
2022-03-11 14:21:21 +0900

    test initialization git repository

---
+++ julia_print_println.jl
@@ -0,0 +1,7 @@
+Sys.WORD_SIZE
+
+print("\" is the symbol for string.")
+println("\' is the symbol for a character, not string as it was in Python.")
+println("\'print\' and \'println\' are different from each other. I'm glad they separate them.")
+
+println("I will post it on new git repository.")
