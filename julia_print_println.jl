f6c3567a137ccd69657c2a22826859023646dd84
DongGun Lee <zlffj1032@gmail.com>
2022-03-11 14:25:34 +0900

    test 2

--- julia_print_println.jl
+++ julia_print_println.jl
@@ -5,3 +5,4 @@ println("\' is the symbol for a character, not string as it was in Python.")
 println("\'print\' and \'println\' are different from each other. I'm glad they separate them.")
 
 println("I will post it on new git repository.")
+println("I think uploading to remote repository is prohibited by the security policy of LG-Innotek.")
