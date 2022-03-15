608c487068ad02004897fd9ceb94fe42e54c17d4
Dong Gun Lee <zlffj1032@gmail.com>
2022-03-15 17:20:25 +0900

    gaussian development

--- FDTD/sources.jl
+++ FDTD/sources.jl
@@ -1,4 +1,6 @@
-using PhysicalConstants.CODATA2018
+using Printf
+#using PhysicalConstants.CODATA2018
+#import PhysicalConstants.CODATA2018: c_0, μ_0, ε_0
 #=
 mutable struct Gaussian
     Δt::Float64
@@ -7,11 +9,32 @@ mutable struct Gaussian
     peak_pos::Float64 # Peak location in time-domain.
 end
 =#
-function gaussian(dt::Float64,cwv::Float64,
-    spread::Float64, peak_pos::Float64)::Float64
-    cfreq = c_0 / cwv
-    return cfreq
+
+c0 = 299792458
+mu0 = 4*π*10^(-7)
+
+um = 1e-6
+nm = 1e-6
+
+Lx, Ly, Lz = 128*10*um, 128*10*um, 128*10*um
+Nx, Ny, Nz = 128, 128, 128
+dx, dy, dz = Lx/Nx, Ly/Ny, Lz/Nz
+
+courant = 1./4
+dt = courant * min(dx,dy,dz) / c0
+Tsteps = 3001
+
+
+function gaussian(tstep, dt, cwv, spread, peak_pos)::Float64
+    cfreq = c0 / cwv
+    w0 = 2*π*cfreq
+    ws = spread *w0
+    ts = 1/ws
+    tc = peak_pos * dt
+    tt = tstep*dt - tc
+    pulse_re = exp((-.5) * ((tt*ws)^2)) * cos(w0*tt)
+    return pulse_re
 end
 
-gauss = gaussian(1,100,5,50)
-println(gauss)
+gauss = gaussian(30, 1.,100.,5.,50.)
+@printf("%e", gauss)
